86eca47a50391d1935c5bbe3f9d373467541b78e
Aluno <aluno@cejoseleitelopes.com.br>
2018-08-31 12:18:00 -0300

    1
    1 - Inicio do Projeto(AV2)

---
+++ Tamagoshi/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Util;
+
+namespace Tamagoshi
+{
+    [Activity(Label = "Tamagoshi", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+        }
+    }
+}
+
